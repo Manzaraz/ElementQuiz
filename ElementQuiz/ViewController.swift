d04029303b93a5f5bef6bab5d855dec12b4909ab
Christian Manzaraz <magnusmanz@gmial.com>
2022-11-02 19:09:03 -0300

    Adding Data: Adding Images, Outlets and code

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateElement() 
+    }
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        answerLabel.text = "?"
     }
 
 
